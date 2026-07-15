1ab6bdbe2b4ccb9ff404a980fbe8df54eb04d584
DEADBYTE <termay10000@gmail.com>
2026-07-16 00:33:53 +0700

    site: add public RIGOS status JSON route

---
+++ functions/api/v1/status.js
@@ -0,0 +1,22 @@
+import {
+  errorResponse,
+  methodNotAllowed,
+  publicStatusResponse,
+  readPublicStatus,
+} from "../../../_lib/status.js";
+
+export async function onRequest(context) {
+  if (context.request.method !== "GET" && context.request.method !== "HEAD") {
+    return methodNotAllowed(["GET", "HEAD"]);
+  }
+
+  try {
+    const response = publicStatusResponse(await readPublicStatus(context.env));
+    if (context.request.method === "HEAD") {
+      return new Response(null, { status: response.status, headers: response.headers });
+    }
+    return response;
+  } catch (error) {
+    return errorResponse(error);
+  }
+}
