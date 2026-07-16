407920c93c6a7c710dd0abf0d93fdf42029bed34
DEADBYTE <termay10000@gmail.com>
2026-07-16 22:52:43 +0700

    status: route signed ingest through multi-rig validator

--- functions/_lib/status.js
+++ functions/_lib/status.js
@@ -1,2 +1,20 @@
-export * from "./status-v2.js";
+export {
+  COMPONENT_IDS,
+  MAX_BODY_BYTES,
+  OBSERVATION_SCHEMA,
+  PUBLIC_NODE_LIMIT,
+  PUBLIC_SCHEMA,
+  StatusError,
+  connectionState,
+  errorResponse,
+  jsonResponse,
+  methodNotAllowed,
+  publicStatusResponse,
+  renderStatusPage,
+  sourceKeyRegistry,
+  validateAndSanitizeObservation,
+  verifySignature,
+  worstComponentStatus,
+} from "./status-v2.js";
+export { acceptObservation } from "./status-multi.js";
 export { readPublicStatus } from "./status-read.js";
